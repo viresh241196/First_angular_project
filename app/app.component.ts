6134f29db1cf0b7f64d9b656a73c6f34986da6b9
viresh241196 <vireshrawool007@gmail.com>
2021-05-08 18:26:28 +0530

    added image tag and its src

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Hello World';
+  title = "Hello World";
+  imgUrl = "../assets/1555572153465.png";
 
-  ngOnInit():void {
-    this.title = "Hello form Bridgelabz"
+  ngOnInit(): void {
+    this.title = "Hello form Bridgelabz";
   }
 }
-
