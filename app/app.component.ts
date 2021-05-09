a23456398be75b589be480e7f7f4da28e5097501
viresh241196 <vireshrawool007@gmail.com>
2021-05-09 12:02:40 +0530

    added method and regex

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,13 +9,25 @@ export class AppComponent {
   title = "Hello World";
   imgUrl = "../assets/1555572153465.png";
   url = "https://www.bridgelabz.com/";
+  userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello form Bridgelabz";
   }
 
-  onClick($event: any){
-    console.log("Save button is clicked",$event)
-    window.open(this.url,"_blank")
+  onClick($event: any) {
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event: any) {
+    console.log("Change Event Occurred", $event.data);
+    const nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
   }
 }
