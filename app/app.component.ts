d3357a3a74cde9ec0b0eb3be896ef073949df849
viresh241196 <vireshrawool007@gmail.com>
2021-05-09 10:09:17 +0530

    added url to open in new tab

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title = "Hello World";
   imgUrl = "../assets/1555572153465.png";
+  url = "https://www.bridgelabz.com/";
 
   ngOnInit(): void {
     this.title = "Hello form Bridgelabz";
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked",$event)
+    window.open(this.url,"_blank")
   }
 }
